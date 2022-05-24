d953288639bbd6f9d3211ca96c7db73637f6d295
prateek <stupidcoder22@gmail.com>
2022-05-24 21:22:50 +0530

    header done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Featured from "../../Components/featured/Featured";
+import Header from "../../Components/header/Header";
 import Navbar from "../../Components/navbar/Navbar";
 import "./home.css";
 
@@ -6,6 +8,16 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        {/* <PropertyList/> */}
+        {/* <h1 className="homeTitle">Homes guests love</h1> */}
+        {/* <FeaturedProperties/> */}
+        {/* <MailList/> */}
+        {/* <Footer/> */}
+      </div>
     </div>
   );
 };
