e65a09ddeb73c904cbbce6eba405b2b4132ef8bb
prateek <stupidcoder22@gmail.com>
2022-05-24 21:29:07 +0530

    featured properties done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Featured from "../../Components/featured/Featured";
+import FeaturedProperties from "../../Components/featuredProperties/FeaturedProperties";
 import Header from "../../Components/header/Header";
 import Navbar from "../../Components/navbar/Navbar";
+import PropertyList from "../../Components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
@@ -12,9 +14,9 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
-        {/* <PropertyList/> */}
-        {/* <h1 className="homeTitle">Homes guests love</h1> */}
-        {/* <FeaturedProperties/> */}
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
         {/* <MailList/> */}
         {/* <Footer/> */}
       </div>
